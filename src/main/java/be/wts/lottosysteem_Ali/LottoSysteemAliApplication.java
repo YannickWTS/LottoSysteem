9b1748b8aed6f388b2e0d6bd49bf5a33843bc80e
YannickWTS <yannickwaterschoot@gmail.com>
2025-06-29 14:20:43 +0200

    Initial commit

---
+++ src/main/java/be/wts/lottosysteem_Ali/LottoSysteemAliApplication.java
@@ -0,0 +1,13 @@
+package be.wts.lottosysteem_Ali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoSysteemAliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoSysteemAliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/wts/lottosysteem_Ali/LottoSysteemAliApplicationTests.java
@@ -0,0 +1,13 @@
+package be.wts.lottosysteem_Ali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoSysteemAliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
