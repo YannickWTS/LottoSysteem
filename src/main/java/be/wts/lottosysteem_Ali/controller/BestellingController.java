4199b4c986f0036409ad57d5f09a72c9eefd4ee5
YannickWTS <yannickwaterschoot@gmail.com>
2025-06-30 01:26:27 +0200

    controller voor bestelling aangemaakt.

---
+++ src/main/java/be/wts/lottosysteem_Ali/controller/BestellingController.java
@@ -0,0 +1,27 @@
+package be.wts.lottosysteem_Ali.controller;
+
+import be.wts.lottosysteem_Ali.model.Bestelling;
+import be.wts.lottosysteem_Ali.service.BestellingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bestelling")
+public class BestellingController {
+    private final BestellingService bestellingService;
+
+    public BestellingController(BestellingService bestellingService) {
+        this.bestellingService = bestellingService;
+    }
+
+    @GetMapping
+    public List<Bestelling> getAllBestellingen() {
+        return bestellingService.getAllBestelling();
+    }
+
+    @PostMapping
+    public long addBestelling(@RequestBody Bestelling bestelling) {
+        return bestellingService.addBestelling(bestelling);
+    }
+}
