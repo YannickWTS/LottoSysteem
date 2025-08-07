3faa929261c262403d4a64393d6792581cd3538c
YannickWTS <yannickwaterschoot@gmail.com>
2025-08-07 13:05:20 +0200

    Package naam correctie

--- src/test/java/be/wts/lottosysteem_Ali/controller/BestellingControllerTest.java
+++ src/test/java/be/wts/lottosysteem_Ali/controller/BestellingControllerTest.java
@@ -1,10 +1,9 @@
-package be.wts.lottosysteem_Ali.Controller;
+package be.wts.lottosysteem_Ali.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.simple.JdbcClient;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.springframework.transaction.annotation.Transactional;
