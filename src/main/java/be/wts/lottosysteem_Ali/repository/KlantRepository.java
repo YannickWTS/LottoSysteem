1a87897571622aa34a648736ab79349c37fac8bb
YannickWTS <yannickwaterschoot@gmail.com>
2025-08-12 11:40:38 +0200

    KlantRepository met findAll, findById en save methods.

--- src/main/java/be/wts/lottosysteem_Ali/repository/KlantRepository.java
+++ src/main/java/be/wts/lottosysteem_Ali/repository/KlantRepository.java
@@ -1,7 +1,13 @@
 package be.wts.lottosysteem_Ali.repository;
 
+import be.wts.lottosysteem_Ali.model.Klant;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Repository
 public class KlantRepository {
@@ -10,5 +16,40 @@ public class KlantRepository {
         this.jdbcClient = jdbcClient;
     }
 
+    public List<Klant> findAll() {
+        var sql = """
+                select id, naam, email
+                from klant
+                order by id
+                """;
+        return jdbcClient.sql(sql)
+                .query(Klant.class)
+                .list();
+    }
+
+    public Optional<Klant> findById(long id) {
+        var sql = """
+                select id, naam, email
+                from klant
+                where id = ?
+                """;
+        return jdbcClient.sql(sql)
+                .param(id)
+                .query(Klant.class)
+                .optional();
+    }
+
+    public long save(Klant klant) {
+        var sql = """
+                insert into klant(naam, email)
+                values (?, ?)
+                """;
+        var keyholder = new GeneratedKeyHolder();
+        jdbcClient.sql(sql)
+                .params(klant.getNaam(), klant.getEmail())
+                .update(keyholder);
+
+        return Objects.requireNonNull(keyholder.getKey()).longValue();
+    }
 
 }
