6ceac52b52a88698b0d24091844833a91ac93369
YannickWTS <yannickwaterschoot@gmail.com>
2025-08-12 15:10:44 +0200

    Delete functie toegevoegd.

--- src/main/java/be/wts/lottosysteem_Ali/controller/BestellingController.java
+++ src/main/java/be/wts/lottosysteem_Ali/controller/BestellingController.java
@@ -2,6 +2,7 @@ package be.wts.lottosysteem_Ali.controller;
 
 import be.wts.lottosysteem_Ali.model.Bestelling;
 import be.wts.lottosysteem_Ali.service.BestellingService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +24,11 @@ public class BestellingController {
     @PostMapping
     public long addBestelling(@RequestBody Bestelling bestelling) {
         return bestellingService.addBestelling(bestelling);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("{id}")
+    public void deleteBestelling(@PathVariable long id) {
+        bestellingService.deleteBestelling(id);
     }
 }
